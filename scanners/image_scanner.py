b549fa116310a7a25c911686cbf8d36f2b9fe784
Teddy W <80352297+theodwya@users.noreply.github.com>
2024-10-08 13:34:48 -0400

    Update image_scanner.py

--- scanners/image_scanner.py
+++ scanners/image_scanner.py
@@ -1,22 +1,9 @@
-import os
-from scanners.tools.trivy import run_trivy_image_scan
-from scanners.tools.grype import run_grype_image_scan
-from scanners.tools.clamav import run_clamav_docker_image_scan
+from .tools.trivy import run_trivy_image_scan
+from .tools.grype import run_grype_image_scan
 
 def scan_docker_image(image_name):
-    """Perform scans on a Docker image using different tools."""
-    results = []
-
-    # Run the Trivy scan
-    trivy_result = run_trivy_image_scan(image_name)
-    results.append(trivy_result)
-
-    # Run the Grype scan
-    grype_result = run_grype_image_scan(image_name)
-    results.append(grype_result)
-
-    # Run the ClamAV scan
-    clamav_result = run_clamav_docker_image_scan(image_name)
-    results.append(clamav_result)
-    
-    return results
+    # Perform Docker image scans using Trivy and Grype.
+    scan_results = []
+    scan_results.append(run_trivy_image_scan(image_name))
+    scan_results.append(run_grype_image_scan(image_name))
+    return scan_results
